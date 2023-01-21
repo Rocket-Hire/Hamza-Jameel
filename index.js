75beb569de16f78825a8f8c47282396a1726fd6f
Omer Khan <omerkhan.nu@gmail.com>
2023-01-21 10:21:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// Given a function which takes an array of words as input, find the common letters among the words and their frequency. Return the letters with their frequency.
+
+// Example 1:
+// Input: words = ["browser","brother","brand"]
+// Output: b:3,r:3,o:2,e:2,r:3
+//beosrrw,behorrt,abdnr
+
+// Example 2:
+// Input: words = ["cat","dog","zebra"]
+// Output: a:2
+
+// Example 3:
+// Input: words = ["car","wish"]
+// Output: ""
+
+// var FindCommonFrequencies = function(arr) {
+const find = "browser";//arr.indexOf(0); //browser
+// for(var i=0 ; i<find.length ; i++){
+
+// }
+const a = find.at(0);
+console.log(a);
+
+
+
+
+// };
+
+
